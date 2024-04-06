c8ce9a89e5d9fb7d29dcad16a0bf3a648e9ab1a6
juancalderonx <juanescalderon12@gmail.com>
2024-04-06 10:19:55 -0500

    feat(): setup nats server

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,6 +3,7 @@ import { z, object } from 'zod';
 
 interface EnvVars {
   PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = object({
@@ -11,12 +12,24 @@ const envsSchema = object({
     .positive()
     .max(65536, `Port should be >= 0 and < 65536`)
     .default(3000),
+
+  NATS_SERVERS: z.array(
+    z.string({
+      required_error: `NATS_SERVERS is required`,
+    }),
+  ),
 });
 
-const envVars = envsSchema.parse(process.env) as EnvVars;
+const envVars = envsSchema.parse({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+}) as EnvVars;
 
 export const Envs = {
   ENVIRONMENT: {
     PORT: envVars.PORT,
+  },
+  NATS: {
+    SERVERS: envVars.NATS_SERVERS,
   },
 };
